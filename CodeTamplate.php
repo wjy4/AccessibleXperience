dcea916c08edd6a43b6e86fcee4f9f66cfdea73a
wjy4 <W1097304549@gmail.com>
2023-06-29 18:21:06 +0100

    Create CodeTamplate.php
    This is code tamplate page, where it provide list of codes that can be view and downloaded

---
+++ CodeTamplate.php
@@ -0,0 +1,71 @@
+
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/CodeTemplateStyle.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/CodeTemplate.css'); ?>">
+
+
+    <div class="wrapper">
+    <aside>
+<div class="navi">
+  <h4>Categories</h4>
+  <ul>
+    <li><a href="/html-code-examples/" class="html-menu"><i class="fab fa-html5"></i> Visual Impairment</a></li>
+    <li><a href="/bootstrap-code-examples/" class="bs-menu"><i class="fab fa-bootstrap"></i> Deaf or hard of hearing.</a></li>
+    <li>---</li>
+    <li><a href="/free-web-books/" class="books-menu"><i class="fas fa-book"></i> More</a></li>
+</ul>
+</div>
+    </aside>
+    <main class="main">
+
+<article class="post-single">
+  <header class="post-header">
+    <h1 class="post-title">Website Accessibility Code</h1>
+    <div class="post-meta">Product By JiaYao, Wang</div>
+  </header>
+  <div class="post-content"><p class="intro">Collection of reuseable <strong>accessibility</strong> code examples. For Visual Impairment</p>
+
+<article class="wrapper-2017">
+  <video src="/assets/img/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.mp4" preload="metadata" loop></video>
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Purpose</h4>
+	  <ul>
+		<li>1.1.1 Non-text Content</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a href="" target="_blank">demo and code</a></li>
+	  <li><a href="">download</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>1.Images, form image buttons, and image map hot spots have appropriate, equivalent alternative text.</p>
+  <p>2.Images that do not convey content, are decorative, or contain content that is already conveyed in text are 
+    given empty alternative text (alt="") or implemented as CSS backgrounds. All linked images have descriptive alternative text.</p>
+    <p></p>
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+  <p><span class="accent">Bootstrap version: </span>5.0.1</p>
+</article>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2687268505112777" crossorigin="anonymous"></script>
+<p><ins class="adsbygoogle"
+style="display:block"
+data-ad-format="fluid"
+data-ad-layout-key="-5w+d4-2x+4l+dk"
+data-ad-client="ca-pub-2687268505112777"
+data-ad-slot="1953526485"></ins></p>
+
