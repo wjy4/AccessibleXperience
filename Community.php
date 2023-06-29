c8e6ca952224ed340dbdee4f27a04c099687730a
wjy4 <W1097304549@gmail.com>
2023-06-29 19:49:20 +0100

    Update Community.php
    Update the community page.  The message area should be white instead of black. Removed some useless information display

--- Community.php
+++ Community.php
@@ -1,19 +1,8 @@
 <nav class="navbar navbar-expand-sm navbar-dark">
     <img src="https://i.imgur.com/CFpa3nK.jpg" width="20" height="20" class="d-inline-block align-top rounded-circle" alt=""> 
-    <a class="navbar-brand ml-2" href="#" data-abc="true">Rib Simpson</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> 
         <span class="navbar-toggler-icon"></span> 
-    </button>
-    <div class="end">
-        <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav">
-                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Work</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Capabilities</a> </li>
-                <li class="nav-item "> <a class="nav-link" href="#" data-abc="true">Articles</a> </li>
-                <li class="nav-item active"> <a class="nav-link mt-2" href="#" data-abc="true" id="clicked">Contact<span class="sr-only">(current)</span></a> </li>
-            </ul>        
-        </div>
-    </div>    
+    </button>  
 </nav>
 <!-- Main Body -->
 <section>
@@ -55,7 +44,7 @@
                     <div class="form-group">
                         <h4>Leave a comment</h4>
                         <label for="message">Message</label>
-                        <textarea name="msg" id=""msg cols="30" rows="5" class="form-control" style="background-color: black;"></textarea>
+                        <textarea name="msg" id=""msg cols="30" rows="5" class="form-control" style="background-color: white;"></textarea>
                     </div>
                     <div class="form-group">
                         <label for="name">Name</label>
