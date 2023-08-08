3f3da3e07899e6c6b2cfc2381e0767f024b17b26
wjy4 <W1097304549@gmail.com>
2023-08-08 16:34:39 +0100

    Update CodeTamplate.php
    Added the new research code template informations

--- CodeTamplate.php
+++ CodeTamplate.php
@@ -1,6 +1,4 @@
-
-
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/CodeTemplateStyle.css'); ?>">
+ <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/CodeTemplateStyle.css'); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/CodeTemplate.css'); ?>">
 
 
@@ -23,24 +21,24 @@
     <h1 class="post-title">Website Accessibility Code</h1>
     <div class="post-meta">Product By JiaYao, Wang</div>
   </header>
-  <div class="post-content"><p class="intro">Collection of reuseable <strong>accessibility</strong> code examples. For Visual Impairment</p>
+  <div class="post-content"><p class="intro">Collection of reuseable <strong>accessibility</strong> code examples. For Visual Impairment.<br> A list of everything you need for visual impairment.</p>
 
 <article class="wrapper-2017">
   <video src="/assets/img/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.mp4" preload="metadata" loop></video>
 	<div class="img-extra"></div>
   <div class="info-box">
 	<div class="info info-author">
-	  <h4>Purpose</h4>
+	  <h4>Name of the Code</h4>
 	  <ul>
-		<li>1.1.1 Non-text Content</li>
+		<li>Provide alt text or descriptions for non-text content</li>
 		<li><time>(Level A)</time></li>
 	  </ul>
 	</div>
   <div class="info info-link">
 	<h4>Links</h4>
 	<ul>
-	  <li><a href="" target="_blank">demo and code</a></li>
-	  <li><a href="">download</a></li>
+	  <li><a href="https://codepen.io/HamzaGourram/pen/XWMEvgr" target="_blank">demo and code</a></li>
+	  <li><a href="/assets/zip/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.zip">download</a></li>
 	</ul>
   </div>
   <div class="info info-frontend">
@@ -52,15 +50,280 @@
   </div>
   <h4 class="about-the-item">About a code</h4>
   <h3>Explanations</h3>
-  <p>1.Images, form image buttons, and image map hot spots have appropriate, equivalent alternative text.</p>
-  <p>2.Images that do not convey content, are decorative, or contain content that is already conveyed in text are 
-    given empty alternative text (alt="") or implemented as CSS backgrounds. All linked images have descriptive alternative text.</p>
+  <p>When a screen-reader reads an image or graphic, it will say “graphic” or “image” and then read the image’s alternative (“alt”) text. If an image doesn’t have alt text, the screen-reader will skip it, so for this reason, it is best to provide alt text for images and graphics. Aim to keep descriptions for video and audio succinct and to the point.
+     Remember, screen readers go line-by-line, so get your point across through your text and alt text in as few words as possible.</p>
     <p></p>
   <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
   <p><span class="accent">Responsive: </span>yes</p>
   <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
-  <p><span class="accent">Bootstrap version: </span>5.0.1</p>
 </article>
+<!-- //////////////////////// -->
+
+
+<article class="wrapper-2017">
+  <video src="/assets/img/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.mp4" preload="metadata" loop></video>
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>Use headings to organize page content</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a href="https://codepen.io/HamzaGourram/pen/XWMEvgr" target="_blank">demo and code</a></li>
+	  <li><a href="/assets/zip/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.zip">download</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>This is a best practice that benefits all users. There are a number of ways in which screen readers that support scannability allow users to skim pages to get an overall impression of a page’s content. One common way to skim a page with a screen reader is to jump from heading to heading. While skimming with a screen reader, users can hear an overview of the page’s key information, then backtrack to read the parts they are most interested in. Unfortunately, too many websites lack headings on pages. Without headings, this method of skimming through content is virtually impossible. Bearing this in mind, 
+    it is a crucial rule that designers organize content with headings that, to the extent possible, represent an accurate overview of the content on the page.</p>
+    <p></p>
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+<!-- //////////////////////// -->
+
+
+<article class="wrapper-2017">
+  <video src="/assets/img/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.mp4" preload="metadata" loop></video>
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>Use descriptive titles for every page</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a href="https://codepen.io/HamzaGourram/pen/XWMEvgr" target="_blank">demo and code</a></li>
+	  <li><a href="/assets/zip/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.zip">download</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>Along with having headings outlining the content within Web pages, sites should always contain titles that describe the topic or purpose of the page. The reason being that screen readers announce the page title (the “title” element in the HTML markup) when first loading a Web page. Users who are visually impaired and need to use a screen reader profit by this, 
+    in that they get to take back the valuable minutes they would have spent scanning a page to determine what kind of content it possesses.</p>
+    <p></p>
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+<!-- //////////////////////// -->
+
+<article class="wrapper-2017">
+  <video src="/assets/img/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.mp4" preload="metadata" loop></video>
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>Use explicit and descriptive labels for links and buttons</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a href="https://codepen.io/HamzaGourram/pen/XWMEvgr" target="_blank">demo and code</a></li>
+	  <li><a href="/assets/zip/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.zip">download</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>Avoid using vague link labels such as “click here.” People who use screen-readers often use a keyboard shortcut to list all the links on a page to navigate more efficiently. Since this list of links has no surrounding text, it creates a contextless state. As such, it’s imperative to create descriptive and explicit link labels that make sense out-of-context. This is a best-practice that benefits all users. 
+    Descriptive link labels can also promote scannability for sighted users, and as a bonus, this helps boost search engine optimization (SEO) for the site.</p>
+    <p></p>
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+<!-- //////////////////////// -->
+
+<article class="wrapper-2017">
+  <img src="https://fuzzymath.com/wp-content/uploads/2016/06/text_size.gif">
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>Allow manual font size adjustment</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a href="https://codepen.io/wjy4/pen/rNQbbPe" target="_blank">demo and code</a></li>
+	  <li><a href="/assets/zip/bootstrap-product-cards/popular-products-section-using-html-css-bootstrap.zip">download</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>By providing users a clear option, whether it’s a slider, a drop-down, a button — anything — to alter the font size,
+     brands and businesses (especially those with content-heavy websites) become friendlier to visually impaired users.</p>
+
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+
+<!-- //////////////////////// -->
+
+
+<article class="wrapper-2017">
+  <img src="https://fuzzymath.com/wp-content/uploads/2016/06/Contrast-940x555.png">
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>Provide sufficient contrast using colors and textures</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a href="https://codepen.io/wjy4/pen/LYXvodJ" target="_blank">demo and code</a></li>
+	  <li><a href="https://webaim.org/resources/contrastchecker/">Color contrast checkers</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>Test colors to make sure they have the proper contrast ratio for readability, 
+    using tools such as the Stark plugin for Sketch. Tools such as this one test combinations of text color on background color, and also take text size into consideration. For example, WCAG 2.0 level AA requires a contrast ratio of 4.5:1 for normal text and 3:1 for large text (i.e., 14 point and bold or larger, or 18 point or larger). Color contrast checkers typically check for compliance with this standard, 
+    so it might be a good idea to bookmark the standards and guidelines WCAG has laid out.</p>
+
+    <strong> The contrast ratio of the black text on the golden yellow background passes with a 10.35:1. The white text on the golden yellow background fails however, with a contrast ratio of 2.03:1.</strong> <p></p>
+
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+
+
+
+
+
+<!-- //////////////////////// -->
+
+
+<article class="wrapper-2017">
+  <img src="https://fuzzymath.com/wp-content/uploads/2016/06/textureexample-e1575665946397-940x374.png">
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>Colors contrast graphs</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+	  <li><a  target="_blank">None</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>when using graphs, consider adding textures or patterns. 
+    These provide an extra layer of scannability and differentiation between data points when the ranges of value, hue, and saturation start to break down.</p>
+    <strong> Adding texture or patterns can aid in differentiating data points in graphs for the colorblind. </strong><p></p>
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+
+
+
+<!-- //////////////////////// -->
+
+
+<article class="wrapper-2017">
+  <img src="https://fuzzymath.com/wp-content/uploads/2016/06/Relyingoncolor-940x358.png">
+	<div class="img-extra"></div>
+  <div class="info-box">
+	<div class="info info-author">
+	  <h4>Name of the Code</h4>
+	  <ul>
+		<li>The use of colour</li>
+		<li><time>(Level A)</time></li>
+	  </ul>
+	</div>
+  <div class="info info-link">
+	<h4>Links</h4>
+	<ul>
+    <li><a href="https://codepen.io/wjy4/pen/JjeVqVm" target="_blank">demo and code</a></li>
+	</ul>
+  </div>
+  <div class="info info-frontend">
+	<h4>Use For</h4>
+	<ul>
+	  <li>HTML / CSS</li>
+	</ul>
+	</div>
+  </div>
+  <h4 class="about-the-item">About a code</h4>
+  <h3>Explanations</h3>
+  <p>When it comes to alerts, warnings, and actionable page elements such as textual links and buttons, clarify that these are meaningful to the user by incorporating more than a simple color change. Almost universally, people understand that underlined blue text is a link. On the flip side, there are still sites out there that underline blue text but do not link to anything.
+     Colorblind users should be able to trust that when the color treatment is taken away, the underline will let them know that it is a link.</strong><p></p>
+  <p><span class="accent">Compatible browsers: </span>Chrome, Edge, Firefox, Opera, Safari</p>
+  <p><span class="accent">Responsive: </span>yes</p>
+  <p><span class="accent">Dependencies: </span>https://webaim.org/standards/wcag/checklist#sc1.1.1</p>
+</article>
+
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2687268505112777" crossorigin="anonymous"></script>
 <p><ins class="adsbygoogle"
 style="display:block"
@@ -68,4 +331,9 @@ data-ad-format="fluid"
 data-ad-layout-key="-5w+d4-2x+4l+dk"
 data-ad-client="ca-pub-2687268505112777"
 data-ad-slot="1953526485"></ins></p>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
 
