4a09b406b0b0053a3cc4fd02577d19fe5282a060
wjy4 <W1097304549@gmail.com>
2023-08-10 21:10:14 +0100

    Create checklist.php
    create the checklist page

---
+++ checklist.php
@@ -0,0 +1,139 @@
+<div class="holder d-flex align-items-center justify-content-center">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/checklist.css'); ?>">
+    <div class="container">
+        <header class="text-center mb-5">
+            <h1 class="display-4">Before launch, double check your website...</h1>
+            <p class="fst-italic text-muted">Designing for accessibility is not designing for a niche user group; it’s designing with a variety of potential users in mind, even those you might not have expected. It’s the role of designers and developers alike to ensure that their tools are built to suit all users
+                Or download the PDF from <a href="/images/checklist.pdf" download>here.</a>
+            </p>
+        </header>
+
+        
+        <div class="row">
+            <div class="col-lg-5 mx-auto">
+                <div class="card rounded border-0 shadow-sm position-relative">
+                    <div class="card-body p-5">
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                            <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Header Structure</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck1" type="checkbox" >
+                            <label class="form-check-label" for="flexCheck1"><span class="fst-italic pl-1">Text hierarchy is clearly established (e.g. H1, H2, etc).</span></label>
+                        </div>
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Links</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexChec2" type="checkbox">
+                            <label class="form-check-label" for="flexChec2"><span class="fst-italic pl-1">Linked in-line text is clearly identifiable from body text.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck3" type="checkbox">
+                            <label class="form-check-label" for="flexCheck3"><span class="fst-italic pl-1">All hyperlinks are clearly phrased to indicate to users where the link leads.</span></label>
+                        </div>
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Images</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck4" type="checkbox">
+                            <label class="form-check-label" for="flexCheck4"><span class="fst-italic pl-1">All images are accompanied by alt text.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck5" type="checkbox">
+                            <label class="form-check-label" for="flexCheck5"><span class="fst-italic pl-1">Alt text is appropriately descriptive for image types.</span></label>
+                        </div>
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Audio & Video</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck6" type="checkbox">
+                            <label class="form-check-label" for="flexCheck6"><span class="fst-italic pl-1">All videos include captions.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck7" type="checkbox">
+                            <label class="form-check-label" for="flexCheck7"><span class="fst-italic pl-1">A transcript of audio content is easily available for users.</span></label>
+                        </div>
+
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Colors</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck8" type="checkbox">
+                            <label class="form-check-label" for="flexCheck8"><span class="fst-italic pl-1">Colors alone are not being used to communicate important messages to users.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck9" type="checkbox">
+                            <label class="form-check-label" for="flexCheck9"><span class="fst-italic pl-1">Patterns or textures are used to indicate visual differences on charts, graphs, etc.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck10" type="checkbox">
+                            <label class="form-check-label" for="flexCheck10"><span class="fst-italic pl-1">Elements have a strong color contrast compliant with WCAG. Use a contrast checker if you’re unsure!</span></label>
+                        </div>
+
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Typography</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck11" type="checkbox">
+                            <label class="form-check-label" for="flexCheck11"><span class="fst-italic pl-1">Typefaces are legible and characters have distinct features from one another.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck12" type="checkbox">
+                            <label class="form-check-label" for="flexCheck12"><span class="fst-italic pl-1">Large bodies of text are left-aligned.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck13" type="checkbox">
+                            <label class="form-check-label" for="flexCheck13"><span class="fst-italic pl-1">Text can be easily resized and the webpage still functions as expected.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck14" type="checkbox">
+                            <label class="form-check-label" for="flexCheck14"><span class="fst-italic pl-1">Line lengths are close to 66 characters per line.</span></label>
+                        </div>
+
+
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Navigation & Site Structure</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck15" type="checkbox">
+                            <label class="form-check-label" for="flexCheck15"><span class="fst-italic pl-1">Navigation is succinct and includes only necessary high-level categories.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck16" type="checkbox">
+                            <label class="form-check-label" for="flexCheck16"><span class="fst-italic pl-1">Website’s code follows navigation best practices.</span></label>
+                        </div>
+
+                        <div class="d-flex align-items-center mb-4 pb-4 border-bottom"><i class="far fa-calendar-alt fa-3x"></i>
+                        <div class="ms-3">
+                                <h4 class="text-uppercase fw-weight-bold mb-0">Forms</h4>
+                            </div>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck17" type="checkbox">
+                            <label class="form-check-label" for="flexCheck17"><span class="fst-italic pl-1">Form fields have clearly defined boundaries and borders.</span></label>
+                        </div>
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" id="flexCheck18" type="checkbox">
+                            <label class="form-check-label" for="flexCheck18"><span class="fst-italic pl-1">Form fields have clear labels.</span></label>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
