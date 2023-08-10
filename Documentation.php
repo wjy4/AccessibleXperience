eada2f2941d333744ee2a90fa1624b874395724e
wjy4 <W1097304549@gmail.com>
2023-08-10 21:09:26 +0100

    Update Documentation.php
    update the documentation links

--- Documentation.php
+++ Documentation.php
@@ -1,4 +1,5 @@
 
+
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/DcoumentationStyle.css'); ?>">
 <div class="container">
   <div class="text-center">
@@ -7,19 +8,19 @@
   <div class="container">
     <div class="card-columns">
       <div class="card">
-        <a href="#">
+        <a href="https://www.w3.org/WAI/tips/designing/">
         <img class="card-img-top" src="https://cdn-images-1.medium.com/max/632/1*HnSxmR5WbFCD98wXgUe3EA.png" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Document 1</h5>
+          <h5 class="card-title">Designing for Web Accessibility</h5>
           <p class="card-text">
-           What should you know about accessibility?
+            What should you know about accessibility ?
           </p>
-          <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>1000<i class="far fa-user"></i> Accessibility <i class="fas fa-calendar-alt"></i>July 3, 2023></p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>1000<i class="far fa-user"></i> Accessibility <i class="fas fa-calendar-alt"></i>July 3, 2023</small></p>
       </div>
         </a>
       </div>
       <div class="card">
-        <a href="#">
+        <a href="/news">
         <img class="card-img-top" src="https://play-lh.googleusercontent.com/jQ3ETPwi-0PLLH6HuUFz13JTKYCf40lZn-TTPccOVPFuZdJA2txX9QKgzn-SZt2uSyLb" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Document 2</h5>
@@ -29,30 +30,30 @@
         </a>
       </div>
       <div class="card">
-        <a href="#">
+        <a href="https://www.w3.org/">
         <img class="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/W3C%C2%AE_Icon.svg/1200px-W3C%C2%AE_Icon.svg.png" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Document 3</h5>
+          <h5 class="card-title">Vsite the W3C world</h5>
           <p class="card-text">Updates from WCAG</p>
           <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>1000<i class="far fa-user"></i> WCAG <i class="fas fa-calendar-alt"></i>July 3, 2023</small></p>
         </div>
         </a>
       </div>
       <div class="card">
-        <a href="#">
+        <a href="https://wave.webaim.org/">
         <img class="card-img-top" src="https://i.ytimg.com/vi/ITUDiTgAZY0/maxresdefault.jpg" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Document 4</h5>
+          <h5 class="card-title">WAVE Website evaluation tool</h5>
           <p class="card-text">Check if your website are compatible with the guidelines</p>
           <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>1000<i class="far fa-user"></i> Guidelines <i class="fas fa-calendar-alt"></i>July 3, 2023</small></p>
         </div>
         </a>
       </div>
       <div class="card">
-        <a href="#">
+        <a href="/checklist">
         <img class="card-img-top" src="https://www.darden.virginia.edu/sites/default/files/styles/full_width_4_3_413px_fallback/public/2019-07/hero_topic_images_Artboard%2012.png?itok=cjRNzw5x" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Document 5</h5>
+          <h5 class="card-title">Web Accessibility Checklist</h5>
           <p class="card-text">Things need to look out when design the website</p>
           <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>1000<i class="far fa-user"></i> Design <i class="fas fa-calendar-alt"></i>July 3, 2023</small></p>
         </div>
@@ -68,17 +69,8 @@
         </div>
         </a>
 
-        <div class="card">
-        <a href="https://fuzzymath.com/resources/web-accessibility-checklist/">
-        <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpvuqNdp2AsgwIS8J4uRtGEm9hJ2pH2pHeHw&usqp=CAU" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Web Accessibility Checklist</h5>
-          <p class="card-text">
-          Before launch, double check that…
-          </p>
-          <p class="card-text"><small class="text-muted"><i class="fas fa-eye"></i>1000<i class="far fa-user"></i> Accessibility <i class="fas fa-calendar-alt"></i>July 3, 2023</small></p>
-      </div>
-        </a>
+
+
       </div>
     </div>
   </div>
